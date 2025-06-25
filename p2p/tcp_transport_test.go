601f03033e3959f531fbdaba5c16e53dd020d19a
ANIRUDH <anirudhnarang0@gmail.com>
2025-06-25 23:25:08 +0530

    added test

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -1,1 +1,17 @@
 package p2p
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestTCPTransport(t *testing.T) {
+    opts := TCPTransportOpts{
+        ListenAddr:    ":3000",
+        HandshakeFunc: NOPHandShake,
+        Decoder:       &DefaultDecoder{},
+    }
+    tr := NewTCPTransport(opts)
+    assert.Equal(t, tr.ListenAddr, ":3000")
+    assert.Nil(t, tr.ListenAndAccept())
+}
